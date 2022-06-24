c7f9e3daf51b8613e07797ebc2bb02237a6ae0f1
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2022-06-23 23:57:42 -0300

    validators

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,38 +3,29 @@ import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
-   
   const LoginScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: AuthBackground(
         child: SingleChildScrollView(
-
           child: Column(
-            children:  [
-
+            children: [
               const SizedBox(height: 250),
               CardContainer(
                 child: Column(
-
                   children: [
-
                     const SizedBox(height: 10),
                     Text('Login', style: Theme.of(context).textTheme.headline4),
-                    const SizedBox(height:  30),
-
+                    const SizedBox(height: 30),
                     const _LoginForm(),
-
-
-
                   ],
                 ),
               ),
-
               const SizedBox(height: 50),
-              const Text('Crear una nueva cuenta', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              const Text('Crear una nueva cuenta',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
               const SizedBox(height: 50),
             ],
           ),
@@ -50,57 +41,57 @@ class _LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-
       child: Form(
-
+        autovalidateMode: AutovalidateMode.onUserInteraction,
         child: Column(
-
           children: [
-
             TextFormField(
-
               autocorrect: false,
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecorations.authInputDecoration(
-                hintText: 'nombreusuario@example.com',
-                labelText: 'Correo electrónico',
-                prefixIcon: Icons.alternate_email_rounded
-              ),
+                  hintText: 'nombreusuario@example.com',
+                  labelText: 'Correo electrónico',
+                  prefixIcon: Icons.alternate_email_rounded),
+              validator: (value) {
+                String pattern =
+                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                RegExp regExp = RegExp(pattern);
+
+                return regExp.hasMatch(value ?? '')
+                    ? null
+                    : 'Ingrese un correo válido';
+              },
             ),
-
             const SizedBox(height: 30),
-
             TextFormField(
-
               autocorrect: false,
               obscureText: true,
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecorations.authInputDecoration(
-                hintText: '*********',
-                labelText: 'Contraseña',
-                prefixIcon: Icons.lock_outline
-
-              ),
+                  hintText: '*********',
+                  labelText: 'Contraseña',
+                  prefixIcon: Icons.lock_outline),
+              validator: (value) {
+                return (value != null && value.length >= 6)
+                    ? null
+                    : 'La contraseña debe ser de 6 caracteres como mínimo';
+              },
             ),
-
             const SizedBox(height: 30),
-
             MaterialButton(
               onPressed: () {},
               color: Colors.deepPurple,
               elevation: 0,
               disabledColor: Colors.grey,
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10)
-              ),
-              child: Container (
-                padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-                child: const Text(
-                  'Iniciar sesión', 
-                  style: TextStyle(color: Colors.white)
-                  )
-                  ),
-        )],
+                  borderRadius: BorderRadius.circular(10)),
+              child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: const Text('Iniciar sesión',
+                      style: TextStyle(color: Colors.white))),
+            )
+          ],
         ),
       ),
     );
