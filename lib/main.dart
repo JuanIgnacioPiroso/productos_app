e95467154243e4001bbf93ed1badd1a1044bf4dc
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2022-06-24 17:57:46 -0300

    productcard

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Productos App',
-      initialRoute: 'login',
+      initialRoute: 'home',
       routes: {
         'login':(_) => const LoginScreen(),
         'home': (_) => const HomeScreen(),
       },
       theme: ThemeData.light().copyWith(
         scaffoldBackgroundColor: Colors.grey.shade300,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          color: Colors.indigo,
+
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Colors.indigo,
+          elevation: 0,
+
+        ),
       ),
     );
   }
